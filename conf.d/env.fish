7a6d1c70a29f066fbb6cedffdbd4a3714be75ba5
tttranquilooo <49392498+tttranquilooo@users.noreply.github.com>
2023-10-28 00:09:33 +0800

    update

--- conf.d/env.fish
+++ conf.d/env.fish
@@ -1,13 +1,14 @@
 # Disable writing .DS_Store (run once)
 # defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool TRUE
 
-# Homebrew
-# set -gx HOMEBREW_INSTALL_FROM_API 1
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# Miscellaneous:
-
-# Append my bin directory to $PATH
 # set -a PATH "$HOME/.cargo/bin"
 set -gx EDITOR vim
+set -gx PAGER "vim -R -M +MANPAGER -"
 set -gx LANG "en_GB.UTF-8"
+
+# Append environment variables that texlive require
+set -a PATH "/opt/texlive/bin/universal-darwin"
+set -gx MANPATH "/usr/share/man:/opt/texlive/texmf-dist/doc/man"
+
+# Homebrew
+eval "$(/opt/homebrew/bin/brew shellenv)"
--- conf.d/functions.fish
+++ conf.d/functions.fish
@@ -1,33 +1,28 @@
 # Command aliases
-function bu --description 'command link'
+function bu --description "command link"
 	set_proxy;
 	and brew up $argv;
 	and brew upgrade -g $argv;
 end
 
-function alist --description 'command link'
-	/Users/tranquilo/bin/alist-darwin-arm64/alist --force-bin-dir $argv
+function alist --description "command link"
+	/opt/alist-darwin-arm64/alist --force-bin-dir $argv
 end
 
-function tlmgr --description 'command link'
-	/Users/tranquilo/Library/texlive/bin/universal-darwin/tlmgr $argv
-end
-
-function activate --description 'Activate python virtual environments'
+function activate --description "Activate python virtual environments"
 	source $argv/bin/activate.fish;
 end
 
-function set_proxy --description 'Set web proxy for terminal'
-	set -gx all_proxy 'http://127.0.0.1:7890';
-	set -gx all_proxy 'http://127.0.0.1:7890';
-	set -gx http_proxy 'http://127.0.0.1:7890';
-	set -gx HTTP_PROXY 'http://127.0.0.1:7890';
-	set -gx https_proxy 'socks5://127.0.0.1:7890';
-	set -gx HTTPS_PROXY 'socks5://127.0.0.1:7890';
-	echo "HTTP and HTTPS proxy has been set";
+function set_proxy --description "Set web proxy for terminal"
+	set -gx all_proxy "http://127.0.0.1:7890";
+	set -gx ALL_PROXY "http://127.0.0.1:7890";
+	set -gx http_proxy "http://127.0.0.1:7890";
+	set -gx HTTP_PROXY "http://127.0.0.1:7890";
+	set -gx https_proxy "socks5://127.0.0.1:7890";
+	set -gx HTTPS_PROXY "socks5://127.0.0.1:7890";
 end
 
-function clean_logs --description 'Clean macOS logs'
+function clean_logs --description "Clean macOS logs"
 	sudo find /var/log/ -name "*.asl*" -depth -exec rm {} \;
 	sudo find /var/log/ -name "*.log*" -depth -exec rm {} \;
 	find $HOME/Library/Logs -name "*.log*" -depth -exec rm {} \;
